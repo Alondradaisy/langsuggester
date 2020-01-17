82ddd8a5e47c5e9c3365378d873ab6e786c973ff
alondra warren <alondra.warren96@gmail.com>
2020-01-17 12:04:56 -0800

    continue working on scripts.js functionality and add a clickable button at the end of the form

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,20 @@ $(document).ready(function) {
     $("#Ruby").show();
 }
 });
+
+$(document).ready(function) {
+  var personal = click("Are you learning to code for personal use or professional opportunities?");
+  
+  if (personal)
+    $("#Ruby").show();
+  } else {
+    $(#"Javascript").show();
+  }
+});
+
+$(document).ready(function) {
+  var yes = click("Do you have prior experience or knowledge with any languages?");
+
+  if (yes)
+  $("#")
+}
