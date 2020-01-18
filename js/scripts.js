1fb35bbc6f9e21f8658b7611da2a540f30cf7672
alondra warren <alondra.warren96@gmail.com>
2020-01-17 16:01:45 -0800

    add more styling to web application, work on scripts.js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -22,5 +22,29 @@ $(document).ready(function) {
   var yes = prompt("Do you have prior experience or knowledge with any languages?");
 
   if (yes)
-  $("#")
+    $("#C#").show();
+} else {
+    $("#Javascript").show();
 }
+});
+
+$(document).ready(function) {
+  var yes = prompt("What do you intend to develop through learning to code?");
+
+  if (mobileGameApp)
+    $("#Swift").show();
+} else {
+    $("#Javascript").show();
+}
+});
+
+$(document).ready(function) {
+  var yes = prompt("Do you prefer elegance or functionality?");
+
+  if (elegance)
+    $("#Ruby").show();
+} else {
+    $("#C#").show();
+}
+});
+
